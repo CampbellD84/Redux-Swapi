50335ede45dfcb197ea5079d342f5248d8697cfe
Douglas Campbell <dacampbelltbone@gmail.com>
2019-06-25 22:25:48 -0400

    App is working; its'  calling API

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -3,19 +3,22 @@ import { connect } from "react-redux";
 
 import { CharacterList } from "../components";
 // import actions
+import { getChars } from "../actions";
 
 class CharacterListView extends React.Component {
-  constructor() {
-    super();
-  }
+  // constructor() {
+  //   super();
+  // }
 
   componentDidMount() {
     // call our action
+    this.props.getChars();
   }
 
   render() {
     if (this.props.fetching) {
       // return something here to indicate that you are fetching data
+      // <h2>Fetching...</h2>
     }
     return (
       <div className="CharactersList_wrapper">
@@ -25,11 +28,20 @@ class CharacterListView extends React.Component {
   }
 }
 
+const mapStateToProps = state => {
+console.log(state);
+  return {
+  fetching: state.charsReducer.fetching,
+  characters: state.charsReducer.characters,
+  error: state.charsReducer.error
+  };
+};
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
 export default connect(
-  null /* mapStateToProps replaces null here */,
+  mapStateToProps /* mapStateToProps replaces null here */,
   {
     /* action creators go here */
+    getChars
   }
 )(CharacterListView);
